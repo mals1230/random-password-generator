91f2567295a8483310ec00edd84ccbff6422c903
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-08-19 19:01:42 -0700

    linked CSS and JS to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,13 @@
 // Assignment code here
 
+var length = Number(prompt("Enter a number between 8 and 128"));
+
+var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz"
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var numb = "1234567890"
+var specChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +23,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
