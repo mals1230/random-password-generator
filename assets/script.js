b3674944c1187b1ac93fd14e954729ab41d373d2
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-08-20 22:53:47 -0700

    still working on prompts

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,12 @@
-// Assignment code here
+// Mal notes - 
 
-// Global Variables
+
+// push() the randomly generated character to a empty array called passwordArray
+
+// return.passwordArray.join()  join takes an array and joins it down into a string
+
+
+// - Global Variables
 var lower = "abcdefghijklmnopqrstuvwxyz".split("");
 console.log(lower);
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
@@ -9,40 +15,60 @@ var numb = "1234567890".split("");
 console.log(numb);
 var specChar = ["!", "\u0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\u005B", "\u005C", "\u005D", "^", "_", "`", "{", "|", "}", "~",];
 console.log(specChar);
-var allChar = [lower + upper + numb + specChar];
-console.log(allChar);
+var passibleChars = []
+var passwordArray = []
 
-  
-var length = prompt("How many characters do you want your password to be? \nMust be between 8 and 128");
-  if (!length)
-    alert("You need to select a number")
+//
 
-    //have to add a refresh/loop here so that they choose yes
-    
-    // then these prompts can run:
-
-    var userLower = confirm("Would you like to include lowercase letters?");
-    var userUpper = confirm("Would you like to include UPPERCASE letters?")
-    var userNumb = confirm("Would you like to include numbers?")
-    var userSpecChar = confirm("Would you like to include special characters?")
-
-If (!userLower && !userUpper && !userNumb && !userSpecChar) 
-  alert("You need to select one of the options")
- 
-
-
-var options = ["lower", "upper", "numb", "specChar"];
-
-// GIVEN NOTES: Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// GIVEN NOTES: Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
-// GIVEN NOTES: Add event listener to generate button
+var length = prompt("How many characters do you want your password to be? \nMust be between 8 and 128");
+function validateUserInput (length) {
+  if (length < 8 || length > 128) { 
+    return null
+  }
+}
+var userLower = confirm("Would you like to include lowercase letters?");
+var userUpper = confirm("Would you like to include UPPERCASE letters?")
+var userNumber = confirm("Would you like to include numbers?")
+var userSpecChar = confirm("Would you like to include special characters?")
+
+if (!userLower && !userUpper && !userNumber && !userSpecChar) {
+  alert("You need to select at least one type of character");
+  return generatePassword();
+}
+
+if (userLower) {
+  possibleChars.concat(specialCharsArray)
+}
+
+if (userUpper) {
+  possibleChars.concat(SpecialNumbsArray)
+}
+
+if (userNumber) {
+  possibleChars.concat(SpecialNumbsArray)
+}
+
+if (userSpecChar) {
+  possibleChars.concat(SpecialNumbsArray)
+}
+
+
+
+// GIVEN NOTES: Write password to the #password input
+
+
+
+
 generateBtn.addEventListener("click", writePassword);
+
+
+// GIVEN NOTES: Assignment code here
+var generateBtn = document.querySelector("#generate");
+// GIVEN NOTES: Add event listener to generate button
+generateBtn.addEventListener("click", function())
