636580be3c511ae3056e499bf9da46d31545b465
Mallory S <110414321+mals1230@users.noreply.github.com>
2022-08-20 20:01:52 -0700

    prompts are working

--- assets/script.js
+++ assets/script.js
@@ -1,26 +1,48 @@
 // Assignment code here
 
-var length = Number(prompt("Enter a number between 8 and 128"));
+// Global Variables
+var lower = "abcdefghijklmnopqrstuvwxyz".split("");
+console.log(lower);
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+console.log(upper);
+var numb = "1234567890".split("");
+console.log(numb);
+var specChar = ["!", "\u0022", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "\u005B", "\u005C", "\u005D", "^", "_", "`", "{", "|", "}", "~",];
+console.log(specChar);
+var allChar = [lower + upper + numb + specChar];
+console.log(allChar);
 
-var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+  
+var length = prompt("How many characters do you want your password to be? \nMust be between 8 and 128");
+  if (!length)
+    alert("You need to select a number")
 
-var lowercase = "abcdefghijklmnopqrstuvwxyz"
-var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var numb = "1234567890"
-var specChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    //have to add a refresh/loop here so that they choose yes
+    
+    // then these prompts can run:
 
-// Get references to the #generate element
+    var userLower = confirm("Would you like to include lowercase letters?");
+    var userUpper = confirm("Would you like to include UPPERCASE letters?")
+    var userNumb = confirm("Would you like to include numbers?")
+    var userSpecChar = confirm("Would you like to include special characters?")
+
+If (!userLower && !userUpper && !userNumb && !userSpecChar) 
+  alert("You need to select one of the options")
+ 
+
+
+var options = ["lower", "upper", "numb", "specChar"];
+
+// GIVEN NOTES: Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// GIVEN NOTES: Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// GIVEN NOTES: Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
